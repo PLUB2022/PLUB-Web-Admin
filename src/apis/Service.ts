176631759d44bac3d5be30c1d41d674932eb0bc4
truewayy <dldudwls888@gmail.com>
2023-03-15 23:33:58 +0900

    feat: 공지사항 상세 api 작성

--- src/apis/Service.ts
+++ src/apis/Service.ts
@@ -1,4 +1,4 @@
-import { NoticeListResponse } from '../interfaces/service';
+import { Notice, NoticeListResponse } from '../interfaces/service';
 import { API_URLS } from './../constants/apiUrls';
 import http from './instance';
 
@@ -8,6 +8,15 @@ export const getNoticeList = async () => {
       API_URLS.SERVICE.NOTICE.ITEM
     );
     return pageResponse.content;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getNoticeDetail = async (id: number) => {
+  try {
+    const data: Notice = await http.get(API_URLS.SERVICE.NOTICE.DETAIL(id));
+    return data;
   } catch (error) {
     console.error(error);
   }
