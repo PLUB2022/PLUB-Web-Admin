a43dbd0b1151276d92700207244c5ca71e7631c8
truewayy <dldudwls888@gmail.com>
2023-03-22 17:09:41 +0900

    feat: 오토 리프레시 로직 추가

--- src/layout/Logined.tsx
+++ src/layout/Logined.tsx
@@ -1,13 +1,20 @@
 import styled from '@emotion/styled';
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Header from '../components/Header';
 import SideBar from '../components/SideBar';
 import { Container, TabTitle } from '../styles/Common';
+import { autoRefresh } from '../utils/autoRefresh';
 import { nowCategory } from '../utils/nowPath';
 
 const Logined = () => {
   const { section, tab } = nowCategory();
+
+  useEffect(() => {
+    const interval = setInterval(autoRefresh, 1000 * 60 * 5);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <>
